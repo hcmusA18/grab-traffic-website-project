bdb2115a05f94c802d31f96632f64b67dd56b49d
Khanh Nguyen <nguyenckhanh71@gmail.com>
2024-05-19 14:29:13 +0700

    add vercel analytic

--- frontend/src/pages/Layout/index.tsx
+++ frontend/src/pages/Layout/index.tsx
@@ -1,9 +1,13 @@
 import { Sidebar } from 'components/Sidebar'
 import { Outlet } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 export const RootLayout = () => {
   return (
     <div className="flex w-full flex-col" style={{ height: 'calc(var(--vh, 1vh) * 100)' }}>
+      <Analytics />
+      <SpeedInsights />
       <Sidebar />
       <Outlet />
     </div>
